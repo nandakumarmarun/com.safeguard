df50681cdb293e7baa1007eb15338a64fe75feb5
nandakumarmarun <nanadakumarmaarun@gmail.com>
2024-06-23 14:19:32 +0530

    1.12.5

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,6 +2,7 @@ var ContextPath = location.protocol + "//" + location.host;
 
 
 $(document).ready(function () {
+  $("#dashboard-container").html("please Waite");
   dashboardBody();
   loadTable();
   setChartData();
@@ -43,28 +44,30 @@ function loadContent(data) {
 function dashboardBody() {
   $("#dashboard-container").html("");
   var dashboardHTML = `
-<div class="row dashboard-cards d-flex justify-content-around">
-  <div id="success-count" class="dash-board-item-1 col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
+<div class="row dashboard-cards d-flex justify-content-around mt-2">
+  <div id="success-count" class="dash-board-item-1 card-motion col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
     <span class="fs-3 fw-1 text-white">SUCCESS</span>
     <h1 class="fs-1 text-white">0</h1>
   </div>
-  <div id="failed-count" class="dash-board-item-2 col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
+  <div id="failed-count" class="dash-board-item-2 card-motion col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
     <span class="fs-3 fw-1 text-white">FAILED</span>
     <h1 class="fs-1 text-white">0</h1>
   </div>
-  <div id="moderate-count" class="dash-board-item-3 col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
+  <div id="moderate-count" class="dash-board-item-3 card-motion col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
     <span class="fs-3 fw-1 text-white">MODERATE</span>
     <h1 class="fs-1 fw-1 text-white">0</h1>
   </div>
-  <div id="chart-Container" class="dash-board-item-4 col-sm-12 col-md-4 col-lg-4 col-xl-11 col-xxl-3">
+  <div id="chart-Container" class="dash-board-item-4 card-motion col-sm-12 col-md-4 col-lg-4 col-xl-11 col-xxl-3">
     <canvas id="chartContainer"></canvas>
   </div>
-  <div class="col-12 d-flex justify-content-around mt-3">
-  <button id="new-test-btn" class="btn btn-success col-12 btnTest">NEW TEST</button>
-</div>
+</div>`;
 
-<div class="col-12">
-  <table class="table table-striped mt-4 align-middle col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
+  dashboardHTML += `<div class="col-12 d-flex justify-content-around mt-3 container">
+  <button id="new-test-btn" class="btn btn-success col-12 btnTest">NEW TEST</button>
+</div>`;
+
+  dashboardHTML += `<div class="col-12 mt-3 container table-responsive">
+  <table class="table table-striped table-custom mt-2 align-middle col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2">
     <thead class="th-1">
       <tr>
         <th scope="col-1" class="bg-success text-center align-middle text-white" style="height: 54px">Company Name</th>
@@ -79,9 +82,8 @@ function dashboardBody() {
       
     </tbody>
   </table>
-</div>
 </div>`;
-
+  $("#dashboard-container").html("");
   $("#dashboard-container").append(dashboardHTML);
 }
 
@@ -104,8 +106,14 @@ function loadchart(data) {
       labels: ["SUCESS", "FAILED", "MODERATE"],
       datasets: [
         {
+          label: 'Security Rate',
           data: data,
           borderWidth: 1,
+          backgroundColor: [
+            'rgba(16, 222, 177, 0.8)', // Color for SUCCESS
+            'rgba(209, 16, 62, 0.8)', // Color for FAILED
+            'rgba(9, 125, 204, 0.8)'  // Color for MODERATE
+          ],
         },
       ],
     },
@@ -160,6 +168,15 @@ function setChartData() {
       "Authorization": "Bearer " + localStorage.getItem("token") // Add the Bearer token here
     },
     success: function (data) {
+      $('.card-motion').css({
+        transform: 'scale(1)',
+      });
+      $('.table-custom').css({
+        transform: 'scale(1)',
+      });
+      $('.btnTest').css({
+        transform: 'scale(1)',
+      });
       $('#success-count h1').text(data.sucessCount);
       $('#moderate-count h1').text(data.moderateCount)
       $('#failed-count h1').text(data.failedcount);
